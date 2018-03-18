c91ab16555c526ac2a868f196d1eaffbad433195
JOSE RAMIREZ <hoyeay5@gmail.com>
2018-03-18 09:14:16 -0500

    Added navbar links and added default header content

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'ruby-on-rails-items', to: 'portfolios#ruby_on_rails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
   resources :blogs do
